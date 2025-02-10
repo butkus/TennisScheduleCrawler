15bd11e99b5f1787921748c5e300e68b3cb3e4d4
butkus <butkus.v@gmail.com>
2025-02-10 08:18:21 +0200

    from work laptop 2025-02-08

--- src/main/java/com/butkus/tenniscrawler/DesiresExplicit.java
+++ src/main/java/com/butkus/tenniscrawler/DesiresExplicit.java
@@ -21,7 +21,6 @@ public class DesiresExplicit {
         //  also, in calendar printout at the end, make a note that there's a booking on holiday (or desire on holiday)
         //  this is perhaps a small overkill, but why not do it like Sony and make UX friendly
 
-        desires.add(new Desire(LocalDate.parse("2025-02-06"), NONE, Court.getIndoorIds()));
         desires.add(new Desire(LocalDate.parse("2025-02-09"), NONE, Court.getIndoorIds()));
         desires.add(new Desire(LocalDate.parse("2025-02-11"), NONE, Court.getIndoorIds()));
         desires.add(new Desire(LocalDate.parse("2025-02-12"), NONE, Court.getIndoorIds()));
@@ -30,6 +29,7 @@ public class DesiresExplicit {
         desires.add(new Desire(LocalDate.parse("2025-02-19"), NONE, Court.getIndoorIds()));
         desires.add(new Desire(LocalDate.parse("2025-02-20"), LATER, Court.getIndoorIds()));
         desires.add(new Desire(LocalDate.parse("2025-02-23"), NONE, Court.getIndoorIds()));
+        desires.add(new Desire(LocalDate.parse("2025-02-25"), LATER, Court.getIndoorIds()));
         desires.add(new Desire(LocalDate.parse("2025-02-26"), NONE, Court.getIndoorIds()));
 
         desires.add(new Desire(LocalDate.parse("2025-03-02"), NONE, Court.getIndoorIds()));
