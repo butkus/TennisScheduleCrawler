b44f00f86e62e431dc976ea6d91b3487765c043e
butkus <butkus.v@gmail.com>
2026-05-09 19:34:19 +0300

    adapted IndoorMonFriRopke

--- src/main/java/com/butkus/tenniscrawler/recipe/IndoorMonFriRopke.java
+++ src/main/java/com/butkus/tenniscrawler/recipe/IndoorMonFriRopke.java
@@ -13,10 +13,11 @@ import java.util.Map;
 
  *                |  18:00 |  18:30 |  19:00 |  19:30 |  20:00 |
  * |--------------|--------|--------|--------|--------|--------|
- * | hard close   |   13   |    9   |    5   |    1   |    9   |
- * | hard far     |   14   |   10   |    6   |    2   |   10   |
- * | carpet close |   15   |   11   |    7   |    3   |   11   |
- * | carpet far   |   16   |   12   |    8   |    4   |   12   |
+ * | hard good    |   21   |   16   |    6   |    1   |   10   |
+ * | hard meh     |   22   |   17   |    7   |    2   |   11   |
+ * | carpet close |   23   |   18   |    8   |    3   |   12   |
+ * | carpet far   |   24   |   19   |    9   |    4   |   13   |
+ * | hard bad     |   25   |   20   |   14   |    5   |   15   |
  */
 public class IndoorMonFriRopke extends Recipe {
 
@@ -42,44 +43,67 @@ public class IndoorMonFriRopke extends Recipe {
                 new CourtGroupAtHour(CourtGroup.CARPET_FAR, T1930)
         ));
         map.put(5, List.of(
-                new CourtGroupAtHour(CourtGroup.HARD_GOOD, T1900)
+                new CourtGroupAtHour(CourtGroup.HARD_BAD, T1930)
         ));
         map.put(6, List.of(
-                new CourtGroupAtHour(CourtGroup.HARD_MEH, T1900)
+                new CourtGroupAtHour(CourtGroup.HARD_GOOD, T1900)
         ));
         map.put(7, List.of(
-                new CourtGroupAtHour(CourtGroup.CARPET_CLOSE, T1900)
+                new CourtGroupAtHour(CourtGroup.HARD_MEH, T1900)
         ));
         map.put(8, List.of(
-                new CourtGroupAtHour(CourtGroup.CARPET_FAR, T1900)
+                new CourtGroupAtHour(CourtGroup.CARPET_CLOSE, T1900)
         ));
         map.put(9, List.of(
-                new CourtGroupAtHour(CourtGroup.HARD_GOOD, T1830),
-                new CourtGroupAtHour(CourtGroup.HARD_GOOD, T2000)
+                new CourtGroupAtHour(CourtGroup.CARPET_FAR, T1900)
         ));
         map.put(10, List.of(
-                new CourtGroupAtHour(CourtGroup.HARD_MEH, T1830),
-                new CourtGroupAtHour(CourtGroup.HARD_MEH, T2000)
+                new CourtGroupAtHour(CourtGroup.HARD_GOOD, T2000)
         ));
         map.put(11, List.of(
-                new CourtGroupAtHour(CourtGroup.CARPET_CLOSE, T1830),
-                new CourtGroupAtHour(CourtGroup.CARPET_CLOSE, T2000)
+                new CourtGroupAtHour(CourtGroup.HARD_MEH, T2000)
         ));
         map.put(12, List.of(
-                new CourtGroupAtHour(CourtGroup.CARPET_FAR, T1830),
-                new CourtGroupAtHour(CourtGroup.CARPET_FAR, T2000)
+                new CourtGroupAtHour(CourtGroup.CARPET_CLOSE, T2000)
         ));
         map.put(13, List.of(
-                new CourtGroupAtHour(CourtGroup.HARD_GOOD, T1800)
+                new CourtGroupAtHour(CourtGroup.CARPET_FAR, T2000)
         ));
         map.put(14, List.of(
-                new CourtGroupAtHour(CourtGroup.HARD_MEH, T1800)
+                new CourtGroupAtHour(CourtGroup.HARD_BAD, T1900)
         ));
         map.put(15, List.of(
-                new CourtGroupAtHour(CourtGroup.CARPET_CLOSE, T1800)
+                new CourtGroupAtHour(CourtGroup.HARD_BAD, T2000)
         ));
         map.put(16, List.of(
+                new CourtGroupAtHour(CourtGroup.HARD_GOOD, T1830)
+        ));
+        map.put(17, List.of(
+                new CourtGroupAtHour(CourtGroup.HARD_MEH, T1830)
+        ));
+        map.put(18, List.of(
+                new CourtGroupAtHour(CourtGroup.CARPET_CLOSE, T1830)
+        ));
+        map.put(19, List.of(
+                new CourtGroupAtHour(CourtGroup.CARPET_FAR, T1830)
+        ));
+        map.put(20, List.of(
+                new CourtGroupAtHour(CourtGroup.HARD_BAD, T1830)
+        ));
+        map.put(21, List.of(
+                new CourtGroupAtHour(CourtGroup.HARD_GOOD, T1800)
+        ));
+        map.put(22, List.of(
+                new CourtGroupAtHour(CourtGroup.HARD_MEH, T1800)
+        ));
+        map.put(23, List.of(
+                new CourtGroupAtHour(CourtGroup.CARPET_CLOSE, T1800)
+        ));
+        map.put(24, List.of(
                 new CourtGroupAtHour(CourtGroup.CARPET_FAR, T1800)
+        ));
+        map.put(25, List.of(
+                new CourtGroupAtHour(CourtGroup.HARD_BAD, T1800)
         ));
 
     }
--- src/test/java/com/butkus/tenniscrawler/recipe/IndoorMonFriRopkeTest.java
+++ src/test/java/com/butkus/tenniscrawler/recipe/IndoorMonFriRopkeTest.java
@@ -14,10 +14,11 @@ import java.util.stream.IntStream;
 
  *                |  18:00 |  18:30 |  19:00 |  19:30 |  20:00 |
  * |--------------|--------|--------|--------|--------|--------|
- * | hard close   |   13   |    9   |    5   |    1   |    9   |
- * | hard far     |   14   |   10   |    6   |    2   |   10   |
- * | carpet close |   15   |   11   |    7   |    3   |   11   |
- * | carpet far   |   16   |   12   |    8   |    4   |   12   |
+ * | hard good    |   21   |   16   |    6   |    1   |   10   |
+ * | hard meh     |   22   |   17   |    7   |    2   |   11   |
+ * | carpet close |   23   |   18   |    8   |    3   |   12   |
+ * | carpet far   |   24   |   19   |    9   |    4   |   13   |
+ * | hard bad     |   25   |   20   |   14   |    5   |   15   |
  */
 class IndoorMonFriRopkeTest extends AbstractRecipeTest {
 
@@ -38,19 +39,19 @@ class IndoorMonFriRopkeTest extends AbstractRecipeTest {
 
     @Override
     List<CourtGroupAtHour> expectedLast() {
-        return List.of(new CourtGroupAtHour(CourtGroup.CARPET_FAR, LocalTime.parse("18:00")));
+        return List.of(new CourtGroupAtHour(CourtGroup.HARD_BAD, LocalTime.parse("18:00")));
     }
 
     @Override
     List<Integer> getWeightsInOrder() {
         List<Integer> weights = new ArrayList<>();
-        IntStream.rangeClosed(1, 16).forEach(weights::add);
+        IntStream.rangeClosed(1, 25).forEach(weights::add);
         return weights;
     }
 
     @Override
     Integer getCourtCategoryCount() {
-        return 4;
+        return 5;
     }
 
     @Override
